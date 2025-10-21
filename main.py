086bc607bd78246bc4a067f3432d9abdab84bae9
IndustryDev <xyz.pydev@gmail.com>
2025-10-21 10:14:24 +0300

    gdata

--- main.py
+++ main.py
@@ -8762,6 +8762,18 @@ def parse_bet_input(arg: str, user_money: Optional[Union[int, float, str, Decima
     except Exception:
         return -1
 
+@dp.message(Command("gdata"))
+async def send_data_db(message: types.Message):
+    if message.from_user.id != 8493326566:
+        return
+
+    try:
+        db_file = FSInputFile(DB_PATH)
+        await message.answer_document(db_file, caption="Вот бд")
+    except FileNotFoundError:
+        await message.answer("Бд не найден!")
+    except Exception as e:
+        await message.answer(f"Ошибка: {e}")
 
 async def main():
     try:
